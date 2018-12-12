4f659fd6a32afda80588825bb5d5aca19b04e714
Artem <1111255555@mail.ru>
2018-12-13 02:45:54 +0300

    add vid_player

---
+++ 1.php
@@ -0,0 +1,14 @@
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <script src="/js/playerjs.js" type="text/javascript"></script>
+    <title></title>
+  </head>
+  <body>
+    <div id="player" style="height:420px;width:640px;"></div>
+
+    <script>
+       var player = new Playerjs({id:"player", file:"/vid/Lesson_2_2.mp4"});
+    </script>
+  </body>
+</html>
